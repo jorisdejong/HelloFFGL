9d5309f94fd4429b781cb375092b57ffd79ac3ef
Joris de Jong <joris@hybridvisuals.nl>
2017-12-29 01:14:39 +0100

    updating comments

--- source/plugins/HelloFFGL/HelloFFGL.cpp
+++ source/plugins/HelloFFGL/HelloFFGL.cpp
@@ -24,8 +24,8 @@ static CFFGLPluginInfo PluginInfo(
 	- "Plugin unique ID". This number is used the identify the plugin internally and needs to be different for each and every FFGL plugin in existence.
 	Is there a single list where you can see all the IDs that are already in use? No. Feel free to organise one. 
 	My plugins start with HV and then have two numbers identifying them. So HV01 is my first plugin. Resolume's plugins start with Axxx
-	- "Plugin name". This is the name as its displayed in Resolume's effect list. By putting FF_SOURCE you will create a source instead of an effect.
-	- "Plugin type". Here you can let Resolume know whether the plugin is a source or effect.
+	- "Plugin name". This is the name as its displayed in Resolume's effect list. 
+	- "Plugin type". Here you can let Resolume know whether the plugin is a source or effect. By putting FF_SOURCE you will create a source instead of an effect.
 	- "Plugin description" and "About" are shown in the Help panel in Resolume.
 	*/
 );
@@ -35,12 +35,12 @@ FFGLPlugin::FFGLPlugin()
 {
 	/*This creates a parameter
 	Instead of using 0 in the beginning, we use our define 'theFirstParam'
-	"Does Nothing" is the name (16 character maximum
+	"Does Nothing" is the name (16 character maximum)
 	FF_TYPE_STANDARD means it's a regular slider
 	and 0.5f is it's default value */
 	SetParamInfo( theFirstParam, "Does Nothing", FF_TYPE_STANDARD, 0.5f);
 
-	//because we set our param to be 0.5f by default, it makes sense that our float variable is also 0.5f
+	//because we set our param to be 0.5f by default, it makes sense that our float variable also starts off at 0.5f
 	aFloat = 0.5f;
 }
 
@@ -58,16 +58,14 @@ FFResult FFGLPlugin::ProcessOpenGL(ProcessOpenGLStruct *pGL)
 	return FF_SUCCESS;
 }
 
-/*SetFloatParameter is called by Resolume when you change a slider in the effect
-Later we'll use the value to do stuff in our plugin, 
-but for now we just need to let Resolume know everything is fine by returning FF_SUCCESS */
+/* SetFloatParameter is called by Resolume when you change a slider in the effect */
 FFResult FFGLPlugin::SetFloatParameter(unsigned int index, float value)
 {
 	//now we use the callback to set our internal float variable, whenever the slider in Resolume is changed
 	switch (index)
 	{
 	case theFirstParam:
-		aFloat = value; //this means we've linked the first parameter to the first 
+		aFloat = value; //this means we've linked the first parameter to the float
 		break;
 	}
 
